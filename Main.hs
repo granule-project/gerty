7e8ad110c110d47181370479d37afe8338ca4986
dominic <dom.orchard@gmail.com>
2021-03-27 21:43:29 +0000

    add help/version and do some renaming internally

--- Main.hs
+++ Main.hs
@@ -31,6 +31,9 @@ import System.Exit
 import qualified System.Clock as Clock
 import Text.Printf
 
+import qualified Data.Version as Version
+import Paths_gerty_main (version)
+
 data Options = Options
   { verbosity :: Verbosity
   , renderOpts :: RenderOptions
@@ -39,6 +42,8 @@ data Options = Options
   , trials      :: Int
   , smtSimplify :: Bool
   , useSMT      :: Bool
+  , showHelp    :: Bool
+  , showVersion :: Bool
   }
 
 
@@ -51,6 +56,8 @@ defaultOptions = Options
   , trials      = 1
   , smtSimplify = False
   , useSMT      = True
+  , showHelp    = False
+  , showVersion = False
   }
 
 
@@ -65,64 +72,122 @@ extractTCOpts opts = defaultTCOpts { TM.benchmark = benchmark opts
 printLog :: RenderOptions -> Verbosity -> TCLog -> IO ()
 printLog ro v l = putStrLn $ pprintShowWithOpts ro (formatLog v l)
 
+outputHelp :: IO ()
+outputHelp = do
+  putStrLn "----------------------------------------------------------------"
+  outputVersion
+  putStrLn "----------------------------------------------------------------\n"
+  flip mapM_ options
+    (\(opt, (desc, _))
+        -> putStrLn $ "  --" ++ opt ++ (replicate (40 - (length opt)) ' ') ++ desc)
+  putStrLn ""
+  return ()
+
+
+outputVersion :: IO ()
+outputVersion = putStrLn $ "Gerty v" <> Version.showVersion version ++ " - graded modal dependent type theory"
+
+options :: [(String, (String, String -> Options -> Either String Options))]
+options = [
+   ("help",            ("Show information on flags"
+                        , \_ o -> Right (o { showHelp = True })))
+
+  ,("version",         ("Show version"
+                        , \_ o -> Right (o { showVersion = True })))
+
+  ,("tyc-optimise",    ("Apply grade-directed optimisation to type checking"
+                        , \_ o -> Right (o { tycOptimise = True })))
+
+  ,("benchmark",       ("Benchmarking: Timing on"
+                        , \_ o -> Right (o { benchmark = True })))
+
+  ,("trials",          ("Benchmarking: Number of times to repeat type checking"
+                        , \x o -> Right (o { trials = read (drop 9 x) })))
+
+  ,("silent",          ("Run in silent mode"
+                        , \_ o -> Right (o { verbosity = verbositySilent })))
+
+  ,("info",            ("Run in more verbose mode"
+                        , \_ o -> Right (o { verbosity = verbosityInfo })))
+
+  ,("no-simplify-smt", ("Don't simplify the SMT theorems"
+                        , \_ o -> Right (o { smtSimplify = False })))
+
+  ,("simplify-smt",    ("Simplify the SMT theorems"
+                        , \_ o -> Right (o { smtSimplify = True })))
+
+  ,("no-smt",          ("Turn off SMT for type checking; use normalisation only"
+                        , \_ o -> Right (o { useSMT = False })))
+
+  ,("use-smt",         ("Turn on SMT for type checking"
+                        , \_ o -> Right (o { useSMT = True })))
+
+  ,("debug",           ("Run in debugging mode (mostly for devs)"
+                        , \_ o -> Right (o { verbosity = verbosityDebug
+                                           , renderOpts = (renderOpts o) { showIdentNumbers = True }})))
+ ]
 
 parseArgs :: [String] -> IO (Options, [String])
 parseArgs args = do
   let (optionArgs, nonOptionArgs) = partition ("--" `isPrefixOf`) args
   opts <- parseArgsWithDefaults defaultOptions optionArgs
   pure (opts, nonOptionArgs)
-  where parseArgsWithDefaults opts [] = pure opts
-        parseArgsWithDefaults opts ("--tyc-optimise":xs) =
-          parseArgsWithDefaults (opts { tycOptimise = True }) xs
-        parseArgsWithDefaults opts ("--benchmark":xs) =
-          parseArgsWithDefaults (opts { benchmark = True }) xs
-        parseArgsWithDefaults opts (x:xs) | "--trials=" `isPrefixOf` x =
-          parseArgsWithDefaults (opts { trials = read (drop 9 x) }) xs
-        parseArgsWithDefaults opts ("--silent":xs) =
-          parseArgsWithDefaults (opts { verbosity = verbositySilent }) xs
-        parseArgsWithDefaults opts ("--info":xs) =
-          parseArgsWithDefaults (opts { verbosity = verbosityInfo }) xs
-        parseArgsWithDefaults opts ("--no-simplify-smt":xs) =
-          parseArgsWithDefaults (opts { smtSimplify = False }) xs
-        parseArgsWithDefaults opts ("--simplify-smt":xs) =
-          parseArgsWithDefaults (opts { smtSimplify = True }) xs
-        parseArgsWithDefaults opts ("--no-smt":xs) =
-          parseArgsWithDefaults (opts { useSMT = False }) xs
-        parseArgsWithDefaults opts ("--use-smt":xs) =
-          parseArgsWithDefaults (opts { useSMT = True }) xs
-        parseArgsWithDefaults opts ("--debug":xs) =
-          parseArgsWithDefaults (opts { verbosity = verbosityDebug, renderOpts = (renderOpts opts) { showIdentNumbers = True } }) xs
-        parseArgsWithDefaults _ (x:_) = do
-          putStrLn $ "unknown option: " <> x
+  where
+    parseArgsWithDefaults opts (('-':'-':optionText):rest) =
+      -- Parsed an options, check the available options
+      case lookup optionText options of
+        Nothing -> do
+          putStrLn $ "unknown option: " <> optionText
           exitFailure
+        Just (_, optionFun)  ->
+          case optionFun optionText opts of
+            -- Didn't provide the option properly
+            Left msg -> do
+              putStrLn msg
+              exitFailure
+            -- Success
+            Right opts' ->
+              parseArgsWithDefaults opts' rest
 
+    parseArgsWithDefaults opts [] =
+      pure opts
+
+    parseArgsWithDefaults _ (opt:_) = do
+      putStrLn $ "unknown option: " <> opt
+      exitFailure
 
 main :: IO ()
 main = do
   (opts, args) <- parseArgs =<< getArgs
   let rOpts   = renderOpts opts
       printLn = putStrLn . pprintShowWithOpts rOpts
-  case args of
-    [] -> putStrLn "Please supply a filename as a command line argument"
-    (fname:_) -> do
-      exists <- doesPathExist fname
-      if not exists
-        then putStrLn $ "File `" <> fname <> "` cannot be found."
-        else do
-          input <- readFile fname
-          exitStatus <- benchmarkTime (benchmark opts) (trials opts) (\isLast -> do
-            res <- runNewCheckerWithOpts (extractTCOpts opts) (Interpreter.runTypeChecker fname input)
-            when isLast $ printLog (renderOpts opts) (verbosity opts) (tcrLog res)
-            case tcrRes res of
-              Left err -> do
-                printLn $ red "Ill-typed."
-                return (Left err)
-              Right _ -> do
-                when isLast $ printLn $ green "Well typed."
-                return $ Right ())
-          case exitStatus of
-            Left err -> printLn (formatError err) >> exitFailure
-            Right () -> exitSuccess
+  if showHelp opts
+    then outputHelp
+    else
+      if showVersion opts
+        then outputVersion
+        else
+          case args of
+            [] -> putStrLn "Please supply a filename as a command line argument OR --help to see options"
+            (fname:_) -> do
+              exists <- doesPathExist fname
+              if not exists
+                then putStrLn $ "File `" <> fname <> "` cannot be found."
+                else do
+                  input <- readFile fname
+                  exitStatus <- benchmarkTime (benchmark opts) (trials opts) (\isLast -> do
+                    res <- runNewCheckerWithOpts (extractTCOpts opts) (Interpreter.runTypeChecker fname input)
+                    when isLast $ printLog (renderOpts opts) (verbosity opts) (tcrLog res)
+                    case tcrRes res of
+                      Left err -> do
+                        printLn $ red "Ill-typed."
+                        return (Left err)
+                      Right _ -> do
+                        when isLast $ printLn $ green "Well typed."
+                        return $ Right ())
+                  case exitStatus of
+                    Left err -> printLn (formatError err) >> exitFailure
+                    Right () -> exitSuccess
 
 -- Benchmarking helpers follow
 
